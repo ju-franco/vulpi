e88a5b2d79db862a337f4aaf7f50edddb8b660d1
Julia <juliamelo24r@gmail.com>
2026-03-17 12:04:56 -0300

    primeiro commit: setup react + vite + firebase

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import { db } from './firebase';
+import { collection, getDocs } from 'firebase/firestore';
+import './App.css'
+
+function App() {
+
+
+}
+
+export default App
